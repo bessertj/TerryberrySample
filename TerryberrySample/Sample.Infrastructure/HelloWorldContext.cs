2edd2956c2e923ba6bf4f391314d5461826aff1d
Jamie Bessert <grwcbessert@gmail.com>
2017-03-28 23:00:24 -0400

    Add mock data class.

---
+++ TerryberrySample/Sample.Infrastructure/HelloWorldContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TerryberrySample.Sample.Domain.Entities;
+
+namespace TerryberrySample.Sample.Infrastructure
+{
+    public class HelloWorldContext
+    {
+        public HelloWorldContext()
+        {
+            for (var i = 0; i <= 9; i++)
+            {
+                HelloWorldEntities.Add(new HelloWorld { Message = "hello world" });
+            }
+        }
+        public List<HelloWorld> HelloWorldEntities { get; set; }
+    }
+}
