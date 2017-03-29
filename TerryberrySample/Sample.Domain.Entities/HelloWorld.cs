4f44844ba27314b0e8ecd314b7721c39e6660563
Jamie Bessert <grwcbessert@gmail.com>
2017-03-28 22:55:55 -0400

    Add Entity and API layers.

---
+++ TerryberrySample/Sample.Domain.Entities/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TerryberrySample.Sample.Domain.Entities
+{
+    public class HelloWorld
+    {
+        public string Message { get; set; }
+    }
+}
