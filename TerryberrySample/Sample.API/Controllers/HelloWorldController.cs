c80a81513ecd24c58048d06c9563e7d09f6571c4
Jamie Bessert <grwcbessert@gmail.com>
2017-03-28 23:07:44 -0400

    Use Autofac in API to map the interface to the repository.

--- TerryberrySample/Sample.API/Controllers/HelloWorldController.cs
+++ TerryberrySample/Sample.API/Controllers/HelloWorldController.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using Autofac;
 using System.Web.Http;
+using TerryberrySample.Sample.Abstract;
 
 namespace TerryberrySample.Sample.API.Controllers
 {
     public class HelloWorldController : ApiController
     {
+        public IHttpActionResult GetMessage()
+        {
+            using (var scope = AutofacConfig.Container.BeginLifetimeScope())
+            {
+                var message = scope.Resolve<IHelloWorldRepository>();
+
+                var result = message.GetAll();
+
+                return Json(result);
+            }
+        }
     }
 }
