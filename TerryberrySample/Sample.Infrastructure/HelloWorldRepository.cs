98e20103b638ca0c79e4bf49d286310f489c7577
Jamie Bessert <grwcbessert@gmail.com>
2017-03-28 22:58:23 -0400

    Add Abstract and Infrastructure layers.

---
+++ TerryberrySample/Sample.Infrastructure/HelloWorldRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TerryberrySample.Sample.Abstract;
+using TerryberrySample.Sample.Domain.Entities;
+
+namespace TerryberrySample.Sample.Infrastructure
+{
+    public class HelloWorldRepository : IHelloWorldRepository
+    {
+        public List<HelloWorld> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
